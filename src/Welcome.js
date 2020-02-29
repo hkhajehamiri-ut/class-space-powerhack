bf08b91a08040cf5dd6b01b4021c180e1f9c1afe
Hilda <hilda.kh.amiri@gmail.com>
2020-02-28 22:43:19 -0800

    modified to show only the message" "

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import {
-  Button,
-  Jumbotron } from 'reactstrap';
 
 function WelcomeContent(props) {
   // If authenticated, greet the user
-  if (props.isAuthenticated) {
     return (
       <div>
-        <h4>Hi {props.user.displayName}! Welcome Back Class Space!</h4>
+        <h10>Hi {props.user.displayName}! Welcome Back Class Space!</h10>
         <p>Use the navigation bar on the side to access the tools</p>
       </div>
     );
   }
-  // Not authenticated, present a sign in button
-  return <Button color="primary" onClick={props.authButtonMethod}>Click here to sign in</Button>;
-}
 
 export default class Welcome extends React.Component {
   render() {
     return (
-      <Jumbotron>
-        {/* <h1>React Graph Tutorial</h1>
-        <p className="lead">This sample app shows how to use the Microsoft Graph API to access Outlook and OneDrive data from React</p> */}
+      <div>
         <WelcomeContent
           isAuthenticated={this.props.isAuthenticated}
           user={this.props.user}
-          authButtonMethod={this.props.authButtonMethod} />
-      </Jumbotron>
+          authButtonMethod={this.props.authButtonMethod}
+          style={{ textAlign: 'centre' }}
+          />
+      </div>
     );
   }
 }
